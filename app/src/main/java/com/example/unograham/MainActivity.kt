ce424ed81e797243ff15c52043d7a63a33f29411
javierf10 <157632748+javierf10@users.noreply.github.com>
2024-03-08 23:19:47 +0100

    pantalla de carga
    implementacion de una pantalla de carga, no funciona el gif de carga todavia

--- app/src/main/java/com/example/unograham/MainActivity.kt
+++ app/src/main/java/com/example/unograham/MainActivity.kt
@@ -12,9 +12,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.unograham.ui.theme.UnoGrahamTheme
 
+
+import android.widget.TextView
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             UnoGrahamTheme {
                 // A surface container using the 'background' color from the theme
