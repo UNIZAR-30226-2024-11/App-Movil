877df6bdb1ece4bc2b2bf7b023e8058a8ee208f7
javierf10 <157632748+javierf10@users.noreply.github.com>
2024-04-16 13:53:26 +0200

    Update TiendaActivity.kt

--- app/src/main/java/com/example/unograham/activities/TiendaActivity.kt
+++ app/src/main/java/com/example/unograham/activities/TiendaActivity.kt
@@ -5,12 +5,19 @@ import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.unograham.R
+import android.widget.ImageView
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
+import com.example.unograham.utils.Preferencias
 
 class TiendaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tienda)
+
+        cargarAvatar(,1)
+        
 
         val backArrowButton = findViewById<ImageButton>(R.id.backArrow)
 
@@ -20,4 +27,47 @@ class TiendaActivity : AppCompatActivity() {
             finish()
         }
     }
+}
+
+
+fun elegirAvatar(avatarImageView: ImageView) {
+    // ENVIAR AL BACKEND UNA ACTUALIZACION DEL AVATAR CADA VEZ QUE SE CAMBIE EN LOCAL O SOLO AL FINAL??
+    val avatarId = avatarImageView.tag as? Int
+    Preferencias.guardarValorEntero("avatar", avatarId)
+}
+
+fun cargarAvatar(avatarImageView: ImageView, avatarId: Int) {
+    avatarImageView.setImageResource(avatarId)
+    avatarImageView.tag = avatarId // Guardar el ID del avatar como una etiqueta en el ImageView
+}
+
+
+fun aplicarEscalaDeGrises(avatar: ImageView) {
+    val matrix = ColorMatrix()
+    matrix.setSaturation(0f) // Configurar la saturación a 0 para obtener una imagen en escala de grises
+
+    val filter = ColorMatrixColorFilter(matrix)
+    avatar.colorFilter = filter
+}
+
+fun avatarToInt(avatarImageView: ImageView) :Int {
+
+    val option = avatarImageView.
+
+    when (option) {
+         -> {
+            return 1
+        }
+         -> {
+            return 2
+        }
+         -> {
+           return 3
+        }
+        else -> {
+            println("Opción no reconocida")
+            // Código para cualquier otro caso
+        }
+    }
+    return
 }
