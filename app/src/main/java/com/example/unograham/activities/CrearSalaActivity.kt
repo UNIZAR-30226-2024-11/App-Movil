ad83a871912caea7be0ac4eeef1bce32473e8785
javierf10 <157632748+javierf10@users.noreply.github.com>
2024-05-15 01:11:34 +0200

    mejoras socket io

--- app/src/main/java/com/example/unograham/activities/CrearSalaActivity.kt
+++ app/src/main/java/com/example/unograham/activities/CrearSalaActivity.kt
@@ -12,38 +12,55 @@ import io.socket.client.IO
 import io.socket.client.Socket
 import org.json.JSONObject
 import java.net.URISyntaxException
+import java.io.IOException
+import android.util.Log
 
-class CrearSalaActivity : AppCompatActivity(){
 
+class CrearSalaActivity : AppCompatActivity() {
+
+    private val TAG = "CrearSalaActivity"
     private val BASE_URL = "https://backend-eg2q.onrender.com/api/"
     private lateinit var socket: Socket
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crear_sala)
 
-            try {
-                socket = IO.socket(BASE_URL)
+        try {
+            Log.e(TAG, "empieza el try")
+            socket = IO.socket(BASE_URL)
+            Log.e(TAG, "socket = IO.socket(Base url)")
+            socket.on(Socket.EVENT_CONNECT) {
+                Log.d(TAG, "Conectado al servidor")
 
-                socket.on(Socket.EVENT_CONNECT) {
-                    Toast.makeText(this@CrearSalaActivity, "conectado al servidor", Toast.LENGTH_SHORT).show()
-                    
-                    val data = JSONObject()
-                    socket.emit("create-lobby", data)
-                }
+                val data = JSONObject()
+                // Aquí puedes incluir datos para crear la sala, si es necesario
+                // data.put("roomName", "Nombre de la Sala")
 
-                socket.connect()
-
-            } catch (e: URISyntaxException) {
-                e.printStackTrace()
+                socket.emit("create-lobby", data)
             }
+            Log.e(TAG, "antes de intentar conexion")
+            socket.connect()
+            Log.e(TAG, "despues de intenter conexion")
+
+        } catch (e: URISyntaxException) {
+            Log.e(TAG, "Error de sintaxis en la URL: ${e.message}")
+            e.printStackTrace()
+        } catch (e: IOException) {
+            Log.e(TAG, "Error de conexión al socket: ${e.message}")
+            e.printStackTrace()
+        } catch (e: Exception) {
+            Log.e(TAG, "Error desconocido: ${e.message}")
+            e.printStackTrace()
         }
 
-        override fun onDestroy() {
-            super.onDestroy()
-            socket.disconnect()
-        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        socket.disconnect()
+    }
 }
-
 
 
 
